dfd9f0f9c98d3c2a569904792d6ad9658b4ad8c0
micproietti <164628747+micproietti@users.noreply.github.com>
2024-05-13 23:26:07 +0200

    fatta la IngredientList per il Modal che mostra un cocktail. Dobbiamo solo sistemare la checheckbox nella CreateCocktailModal class e migliorare la grafica

--- cocktailbarapp/lib/pages/cocktail/elements/CocktailElementModal.dart
+++ cocktailbarapp/lib/pages/cocktail/elements/CocktailElementModal.dart
@@ -1,18 +1,125 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cocktailbarapp/pages/cocktail/elements/IngredientlList.dart';
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
 
 // ignore: must_be_immutable
+
 class CocktailElementModal extends StatelessWidget {
   final String idElemento;
-  CocktailElementModal({super.key, required this.idElemento});
 
-  final FirebaseFirestore db = FirebaseFirestore.instance;
-  
+  CocktailElementModal({required this.idElemento});
+
+  late CollectionReference ingredients = FirebaseFirestore.instance.collection('Cocktails')
+                                    .doc(idElemento)
+                                    .collection('ingredients');
 
   @override
   Widget build(BuildContext context) {
     return Dialog(
+      child: Container(
+       width: MediaQuery.of(context).size.width *
+            0.8, // 70% della larghezza dello schermo
+        height: MediaQuery.of(context).size.height *
+            0.8, // Altezza fissa o percentuale
+        padding: const EdgeInsets.all(20.0),
+        child: FutureBuilder(
+      future: FirebaseFirestore.instance.collection('Cocktails').doc(idElemento).get(),
+      builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(), // Indicatore di caricamento
+          );
+        }
+
+        if (snapshot.hasError) {
+          return Container(
+            alignment: Alignment.center,
+            child: Text('Errore: ${snapshot.error}'),
+          );
+        }
+
+        if (!snapshot.hasData || !snapshot.data!.exists) {
+          return Container(
+            alignment: Alignment.center,
+            child: Text('Nessun cocktail trovato con l\'ID fornito.'),
+          );
+        }
+
+        var data = snapshot.data!.data() as Map<String, dynamic>;
+
+        return Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(data['name'] ?? 'Nome del cocktail', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              SizedBox(height: 10),
+              Text('Metodo: ${data['method'] ?? 'Nessun metodo specificato'}'),
+              Text('Garnish: ${data['garnish'] ?? 'Nessun garnish specificato'}'),
+              Text('Ghiaccio: ${data['ice'] ?? false ? 'Si' : 'No'}'),
+              SizedBox(height: 20),
+              Text('Ingredienti:', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              Expanded(
+                child: IngredientList(ingredientsCollection: ingredients),
+              ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Chiudi'),
+                  ),
+                ),
+            ],
+          ),
+        );
+      },
+    ),
+    ),
+    );
+  }
+}
+
+
+class DeleteCocktailElementButton extends StatefulWidget {
+  final String idElemento;
+  const DeleteCocktailElementButton({super.key, required this.idElemento});
+
+  @override
+  State<DeleteCocktailElementButton> createState() =>
+      _DeleteCocktailElementButtonState();
+}
+
+class _DeleteCocktailElementButtonState
+    extends State<DeleteCocktailElementButton> {
+  @override
+  Widget build(BuildContext context) {
+    return GFButton(
+      onPressed: () async {
+        try {
+          // Esegui una query per trovare il documento che corrisponde alle condizioni specificate
+          await FirebaseFirestore.instance
+              .collection('Cocktails')
+              .doc(widget.idElemento)
+              .delete();
+
+          // Se esiste un documento che corrisponde alle condizioni, elimina il documento
+        } catch (error) {
+          print('Errore durante l\'eliminazione dell\'elemento: $error');
+        }
+        Navigator.of(context).pop();
+      },
+      color: Colors.purple,
+      text: 'Cancella Elemento',
+      textColor: Colors.white,
+    );
+  }
+}
+
+
+/*return Dialog(
       child: Container(
        width: MediaQuery.of(context).size.width *
             0.7, // 70% della larghezza dello schermo
@@ -110,41 +217,4 @@ class CocktailElementModal extends StatelessWidget {
         },
       ),
       ),
-    );
-  }
-}
-
-class DeleteCocktailElementButton extends StatefulWidget {
-  final String idElemento;
-  const DeleteCocktailElementButton({super.key, required this.idElemento});
-
-  @override
-  State<DeleteCocktailElementButton> createState() =>
-      _DeleteCocktailElementButtonState();
-}
-
-class _DeleteCocktailElementButtonState
-    extends State<DeleteCocktailElementButton> {
-  @override
-  Widget build(BuildContext context) {
-    return GFButton(
-      onPressed: () async {
-        try {
-          // Esegui una query per trovare il documento che corrisponde alle condizioni specificate
-          await FirebaseFirestore.instance
-              .collection('Cocktails')
-              .doc(widget.idElemento)
-              .delete();
-
-          // Se esiste un documento che corrisponde alle condizioni, elimina il documento
-        } catch (error) {
-          print('Errore durante l\'eliminazione dell\'elemento: $error');
-        }
-        Navigator.of(context).pop();
-      },
-      color: Colors.purple,
-      text: 'Cancella Elemento',
-      textColor: Colors.white,
-    );
-  }
-}
+    );*/
