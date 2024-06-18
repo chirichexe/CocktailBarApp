a78fdc98f0760bdcdeae7c130af736a712e6485b
micproietti <164628747+micproietti@users.noreply.github.com>
2024-06-18 18:57:28 +0200

    sborra

--- Cocktail Bar App/lib/elements/ShouldDeleteDialog.dart
+++ Cocktail Bar App/lib/elements/ShouldDeleteDialog.dart
@@ -10,39 +10,52 @@ class ShouldDeleteDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text('Conferma Cancellazione',
-                  style: TextStyle(
-                  color: Colors.blueAccent,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold)),
+          style: TextStyle(
+              color: Colors.blueAccent,
+              fontSize: 18,
+              fontWeight: FontWeight.bold)),
       content: const Text('Sei sicuro di voler cancellare questo elemento?',
-                          style: TextStyle(
-                          color: Colors.blueGrey,
-                          fontSize: 18,
-                          fontWeight: FontWeight.normal)),
+          style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 18,
+              fontWeight: FontWeight.normal)),
       actions: <Widget>[
         TextButton(
           child: const Text('No',
-                      style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold)),
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold)),
           onPressed: () {
             Navigator.of(context).pop(false);
           },
         ),
         TextButton(
           child: const Text('Sì',
-                            style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold)),
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold)),
           onPressed: () async {
-            await documentReference.delete();
+            await _deleteDocumentWithSubcollections(documentReference);
             Navigator.of(context).pop(true);
           },
         ),
       ],
     );
+  }
+
+  static Future<void> _deleteDocumentWithSubcollections(DocumentReference document) async {
+    // Fetch all collections of the document
+    var subcollections = await document.firestore.collectionGroup(document.id).get();
+
+    // Iterate through each collection and delete its documents
+    for (var collection in subcollections.docs) {
+      await _deleteDocumentWithSubcollections(collection.reference);
+    }
+
+    // Delete the document itself
+    await document.delete();
   }
 
   static Future<bool?> showDeleteDialog(
--- Cocktail Bar App/lib/pages/magazzino/navigator/elements/MagazzinoElementModal.dart
+++ Cocktail Bar App/lib/pages/magazzino/navigator/elements/MagazzinoElementModal.dart
@@ -26,71 +26,74 @@ class MagazzinoElementModal extends StatelessWidget {
       ),
       child: IntrinsicHeight(
         child: Container(
-          width: MediaQuery.of(context).size.width * 0.5,
+          width: MediaQuery.of(context).size.width * 0.8,
           padding: const EdgeInsets.all(20.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          nome,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24,
-                            color: Colors.blueAccent,
-                          ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        nome,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                          color: Colors.blueAccent,
                         ),
-                        const SizedBox(height: 10),
-                        Text(
-                          descrizione,
-                          style: const TextStyle(
-                            fontSize: 16,
-                            color: Colors.blueGrey,
-                          ),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        descrizione,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.blueGrey,
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
-              const Spacer(),
+              const SizedBox(height: 20),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  GFButton(
-                    onPressed: () async {
-                      bool? shouldDelete =
-                          await ShouldDeleteDialog.showDeleteDialog(
-                        context,
-                        FirebaseFirestore.instance
-                            .collection('Magazzini')
-                            .doc(idMag)
-                            .collection('Elements')
-                            .doc(idEl),
-                      );
+                  Flexible(
+                    child: GFButton(
+                      onPressed: () async {
+                        bool? shouldDelete =
+                            await ShouldDeleteDialog.showDeleteDialog(
+                          context,
+                          FirebaseFirestore.instance
+                              .collection('Magazzini')
+                              .doc(idMag)
+                              .collection('Elements')
+                              .doc(idEl),
+                        );
 
-                      if (shouldDelete == true) {
-                        Navigator.of(context).pop();
-                      }
-                    },
-                    text: 'Elimina',
-                    color: Colors.redAccent,
-                    icon: Icon(Icons.delete, color: Colors.white),
-                    shape: GFButtonShape.pills,
+                        if (shouldDelete == true) {
+                          Navigator.of(context).pop();
+                        }
+                      },
+                      text: 'Elimina',
+                      color: Colors.redAccent,
+                      icon: Icon(Icons.delete, color: Colors.white),
+                      shape: GFButtonShape.pills,
+                    ),
                   ),
-                  GFButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    text: 'Chiudi',
-                    color: Colors.blue,
-                    shape: GFButtonShape.pills,
+                  const SizedBox(width: 10), // Adding some spacing between buttons
+                  Flexible(
+                    child: GFButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      text: 'Chiudi',
+                      color: Colors.blue,
+                      shape: GFButtonShape.pills,
+                    ),
                   ),
                 ],
               ),
